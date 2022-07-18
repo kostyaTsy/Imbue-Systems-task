de8ccd59d02952e12c6bb6e1fbf24dbd41a7cd7c
Kostya <tsyvilko12@gmail.com>
2022-07-18 22:04:00 +0300

    Initial Commit

---
+++ Imbue Systems task/Imbue_Systems_taskApp.swift
@@ -0,0 +1,17 @@
+//
+//  Imbue_Systems_taskApp.swift
+//  Imbue Systems task
+//
+//  Created by Kostya Tsyvilko on 18.07.22.
+//
+
+import SwiftUI
+
+@main
+struct Imbue_Systems_taskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
